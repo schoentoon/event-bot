21d83cad7eac39a4451c69c29affe177617c1c9c
Toon Schoenmakers <nighteyes1993@gmail.com>
2019-06-01 09:58:28 +0200

    Changed the database schema to include a drafts and user state table

--- db.go
+++ db.go
@@ -25,10 +25,33 @@ func UpgradeDatabase(db *sql.DB) error {
 	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS public.events (
 		id serial NOT NULL,
 		"owner" bigint NOT NULL,
-		insert_state insert_state DEFAULT 'waiting_for_name',
 		name varchar NULL,
 		description varchar NULL,
 		CONSTRAINT events_pk PRIMARY KEY (id)
+	);`)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(`CREATE TYPE user_state AS ENUM ('no_command', 'waiting_for_event_name', 'waiting_for_description')`)
+	if err != nil {
+		log.Printf("%v, continueing anyway..", err)
+	}
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS public.user_states (
+		user_id bigint NOT NULL,
+		state user_state DEFAULT 'no_command',
+		CONSTRAINT user_id_pk PRIMARY KEY (user_id)
+	);`)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS public.drafts (
+		user_id bigint NOT NUL,
+		name varchar NULL,
+		description varchar NULL,
+		CONSTRAINT user_id_pk PRIMARY KEY (user_id)
 	);`)
 	if err != nil {
 		return err
